bd03f562dcada1e13392fe9d9d59c82f7e845636
mp926 <mp926@scarletmail.rutgers.edu>
2020-02-12 16:07:02 -0500

    Added code to check if HYDRUS is implementing scaling factors correctly

---
+++ HYDRUS scaling factor check.R
@@ -0,0 +1,4 @@
+# Checking the scaling factor implementation in HYDRUS vs measured data
+# Hypothesis: HYDRUS takes the water retention characteristic curve and multiplies it by the 
+# given scaling factor at each node
+
