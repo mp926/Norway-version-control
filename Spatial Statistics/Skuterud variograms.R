b53143165d6a5ec79a4fa7bf3ca802262c53dee6
mp926 <mp926@scarletmail.rutgers.edu>
2020-02-16 16:12:50 -0500

    removed width and range limits on Ks variograms to try and get the model to fit

--- Spatial Statistics/Skuterud variograms.R
+++ Spatial Statistics/Skuterud variograms.R
@@ -67,7 +67,7 @@ coordinates(df.adj[[i]])<- ~x+y+z #convert each data.frame to geoObject for gsta
 
 vth[[i]]<-variogram(sfth~1,df.adj[[i]], width=13, cutoff=60) #calculate the variograms for each iteration
 vh[[i]]<-variogram(sfh~1,df.adj[[i]], width=13, cutoff=60)
-vK[[i]]<-variogram(Ks~1,df.adj[[i]], width=13, cutoff=60)
+vK[[i]]<-variogram(Ks~1,df.adj[[i]], cutoff=60)
 ridxth<-first((which(vth[[i]]$gamma>(var(df.adj[[i]]$sfth)-(var(df.adj[[i]]$sfth)*0.15))))) 
 ridxh<-first((which(vh[[i]]$gamma>(var(df.adj[[i]]$sfh)-(var(df.adj[[i]]$sfh)*0.15))))) 
 ridxK<-first((which(vK[[i]]$gamma[2:length(vK[[i]]$gamma)]>(var(df.adj[[i]]$Ks)-(var(df.adj[[i]]$Ks)*0.15))))) 
@@ -81,7 +81,7 @@ vth.fit[[i]]<-fit.variogram(vth[[i]], vgm(psill=var(df.adj[[i]]$sfth), "Exp", ra
                                           nugget=vth[[i]]$gamma[1]/1.2)) #fit each variogram
 vh.fit[[i]]<-fit.variogram(vh[[i]], vgm(psill=var(df.adj[[i]]$sfh), "Sph", range=init.rangeh[i,1],
                                           nugget=vh[[i]]$gamma[1]/1.2))
-vK.fit[[i]]<-fit.variogram(vK[[i]], vgm(psill=var(df.adj[[i]]$Ks), "Exp", range=1,
+vK.fit[[i]]<-fit.variogram(vK[[i]], vgm(psill=var(df.adj[[i]]$Ks), "Lin", range=init.rangeK[i,1],
                                         nugget=vK[[i]]$gamma[1]/1.2), fit.ranges=FALSE)
 
 SSEth[i,1]<-attr(vth.fit[[i]],"SSErr") #obtain the sum square errors of each fitted variogram
@@ -90,7 +90,7 @@ SSEK[i,1]<-attr(vh.fit[[i]],"SSErr")
 
 vmodel.linesth[[i]]<-variogramLine(vth.fit[[i]],maxdist=60,n=200) # simulate model line output for plotting
 vmodel.linesh[[i]]<-variogramLine(vh.fit[[i]],maxdist=60,n=200)
-vmodel.linesK[[i]]<-variogramLine(vK.fit[[i]],maxdist=60,n=200)
+vmodel.linesK[[i]]<-variogramLine(vK.fit[[i]],maxdist=100,n=200)
 }
 
 
@@ -136,7 +136,7 @@ for (i in 1:n){
 }
 
 
-coordinates(df)<- ~x+y+z
+coordinates(df)<- ~x+y+z  # This transforms the data.frame. If you want to use df again, you must reload the data.frame into the environment
 vth.orig=variogram(sfth~1,df, width=13, cutoff=60) #cutoff = distance where np first decreases
 vth.orig.fit<-fit.variogram(vth.orig,vgm(psill=0.06,"Exp",range=45, nugget=0.10),fit.ranges=FALSE)
 origmodel.lines.th<-variogramLine(vth.orig.fit,maxdist=60,n=200)
@@ -145,9 +145,9 @@ vh.orig=variogram(sfh~1,df, width=13, cutoff=60) #cutoff = distance where np fir
 vh.orig.fit<-fit.variogram(vh.orig,vgm(psill=0.12,"Sph",range=20, nugget=0.08))
 origmodel.lines.h<-variogramLine(vh.orig.fit,maxdist=60,n=200)
 
-vK.orig=variogram(Ks~1,df, width=13, cutoff=60) #cutoff = distance where np first decreases
+vK.orig=variogram(Ks~1,df) #cutoff = distance where np first decreases
 vK.orig.fit<-fit.variogram(vK.orig,vgm(psill=100000,"Exp",range=1, nugget=400000))
-origmodel.lines.K<-variogramLine(vK.orig.fit,maxdist=60,n=200)
+origmodel.lines.K<-variogramLine(vK.orig.fit,maxdist=100,n=200)
 
 
 
@@ -175,7 +175,7 @@ lines(dist.h,origmodel.lines.h$gamma-error99h, lty=6, lwd=2)
 lines(dist.h,origmodel.lines.h$gamma+error99h, lty=6, lwd=2)
 legend("bottomright", legend=c("90% C.I.", "95% C.I.", "99% C.I."), lty=c(2,5,6))
 
-plot(vK.orig$dist,vK.orig$gamma, xlim=c(0,60), ylim=c(350000,700000), pch=16,
+plot(vK.orig$dist,vK.orig$gamma, pch=16,
      ylab=expression(gamma), xlab="lag distance (cm)", main=expression("Variogram" ~ K[s] ~ "[cm/d]"))
 lines(origmodel.lines.K$dist,origmodel.lines.K$gamma, col="red")
 lines(dist.K,origmodel.lines.K$gamma-error90K, lty=2, lwd=2)
@@ -184,7 +184,7 @@ lines(dist.K,origmodel.lines.K$gamma-error95K, lty=5, lwd=2)
 lines(dist.K,origmodel.lines.K$gamma+error95K, lty=5, lwd=2)
 lines(dist.K,origmodel.lines.K$gamma-error99K, lty=6, lwd=2)
 lines(dist.K,origmodel.lines.K$gamma+error99K, lty=6, lwd=2)
-legend("bottomright", legend=c("90% C.I.", "95% C.I.", "99% C.I."), lty=c(2,5,6))
+legend("topright", legend=c("90% C.I.", "95% C.I.", "99% C.I."), lty=c(2,5,6))
 
 
 
